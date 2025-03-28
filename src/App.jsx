c1de13d35cc6662b0d01a65aef60edc144645681
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-03-28 13:25:28 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Akhil</h1>
+     <h2>Contact details:</h2>
+     <h2>vysyaraju.akhilraju.s89@kalvium.community</h2>
+    </>
+  )
+}
+
+export default App
